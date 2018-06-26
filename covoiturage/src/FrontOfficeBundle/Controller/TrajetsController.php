1469ebdd4b8bee089347c45ee6a3dd6b08fc24f0
nd4pa <antoine.blondeau@isen-bretagne.fr>
2018-06-26 12:12:08 +0200

    Print first trajects

--- covoiturage/src/FrontOfficeBundle/Controller/TrajetsController.php
+++ covoiturage/src/FrontOfficeBundle/Controller/TrajetsController.php
@@ -10,8 +10,18 @@ class TrajetsController extends Controller
     {
         $em = $this->get('doctrine')->getManager();
         $repository = $em->getRepository('FrontOfficeBundle:Trajet');
+        /*$queryBuilder=$em->createQueryBuilder();
+        $query=$queryBuilder->select('t.id,t.nb_km,t.date,i.nom,i.prenom,d.ville,a.ville')
+                              ->from('FrontOfficeBundle:Trajet', 't')
+                              ->leftJoin('FrontOfficeBundle:Internaute', 'i')
+                              ->leftJoin('FrontOfficeBundle:Ville', 'd')
+                              ->leftJoin('FrontOfficeBundle:Ville1', 'a')
+                              ->where('i.id = :internaute_id AND d.id = :ville_id AND a.id = :ville_id1')
+                              ->getQuery();*/
         $trajets = $repository->findAll();
-        //$tab=['aaa', 'bbb', 'ccc'];
+        foreach($trajets as $trajet){
+          $trajet->setDate(date_format($trajet->getDate(),'Y-m-d H:i:s'));
+        }
         return $this->render('FrontOfficeBundle:Trajets:trajets.html.twig', [ 'trajets' => $trajets ]);
     }
 }
