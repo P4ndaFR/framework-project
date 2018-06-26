ef9f4a1d12b09ad4a4a40f44ee79d6960d9a657a
nd4pa <antoine.blondeau@isen-bretagne.fr>
2018-06-26 12:49:09 +0200

    List trajects

--- covoiturage/src/FrontOfficeBundle/Controller/TrajetsController.php
+++ covoiturage/src/FrontOfficeBundle/Controller/TrajetsController.php
@@ -10,17 +10,10 @@ class TrajetsController extends Controller
     {
         $em = $this->get('doctrine')->getManager();
         $repository = $em->getRepository('FrontOfficeBundle:Trajet');
-        /*$queryBuilder=$em->createQueryBuilder();
-        $query=$queryBuilder->select('t.id,t.nb_km,t.date,i.nom,i.prenom,d.ville,a.ville')
-                              ->from('FrontOfficeBundle:Trajet', 't')
-                              ->leftJoin('FrontOfficeBundle:Internaute', 'i')
-                              ->leftJoin('FrontOfficeBundle:Ville', 'd')
-                              ->leftJoin('FrontOfficeBundle:Ville1', 'a')
-                              ->where('i.id = :internaute_id AND d.id = :ville_id AND a.id = :ville_id1')
-                              ->getQuery();*/
         $trajets = $repository->findAll();
         foreach($trajets as $trajet){
-          $trajet->setDate(date_format($trajet->getDate(),'Y-m-d H:i:s'));
+          $trajet->setDate(date_format($trajet->getDate(),'Y-m-d H:i'));
+          //$trajet->internaute = $trajet->getInternaute()->getNom();
         }
         return $this->render('FrontOfficeBundle:Trajets:trajets.html.twig', [ 'trajets' => $trajets ]);
     }
