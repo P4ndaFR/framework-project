8cbeff04ef5532a9afbb24600cc5ae1dcc0eebd4
art35rennes <arthur2rennes@hotmail.fr>
2018-06-26 10:58:00 +0200

    add back office

--- covoiturage/app/AppKernel.php
+++ covoiturage/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FrontOfficeBundle\FrontOfficeBundle(),
             new ApiBundle\ApiBundle(),
+            new covoiturage\BackOfficeBundle\covoiturageBackOfficeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
