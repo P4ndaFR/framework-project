9abba309b12d7c114ff715ee080eecda33d0a1b7
nd4pa <antoine.blondeau@isen-bretagne.fr>
2018-06-26 13:13:08 +0200

    Add Internaute views and link

--- covoiturage/src/FrontOfficeBundle/Controller/TrajetsController.php
+++ covoiturage/src/FrontOfficeBundle/Controller/TrajetsController.php
@@ -13,8 +13,19 @@ class TrajetsController extends Controller
         $trajets = $repository->findAll();
         foreach($trajets as $trajet){
           $trajet->setDate(date_format($trajet->getDate(),'Y-m-d H:i'));
-          //$trajet->internaute = $trajet->getInternaute()->getNom();
         }
         return $this->render('FrontOfficeBundle:Trajets:trajets.html.twig', [ 'trajets' => $trajets ]);
+    }
+    public function showInternauteAction($id)
+    {
+        $em = $this->get('doctrine')->getManager();
+        $repository = $em->getRepository('FrontOfficeBundle:Internaute');
+        $internaute = $repository->find($id);
+        if (!$internaute) {
+          throw $this->createNotFoundException(
+            'No internaute found for id '.$internaute
+          );
+        }
+        return $this->render('FrontOfficeBundle:Trajets:internaute.html.twig', [ 'internaute' => $internaute ]);
     }
 }
